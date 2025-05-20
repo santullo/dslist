9dff444918c63f1a093b501d2e9269e776be4feb
Gabriel Santullo Rocha <rochasantullo@gmail.com>
2025-05-20 18:59:23 -0300

    Properties, Game Entity, ORM

---
+++ src/main/java/com/gamehub/dslist/entities/Game.java
@@ -0,0 +1,35 @@
+package com.gamehub.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_GAME")
+public class Game implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "GAME_YEAR")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
